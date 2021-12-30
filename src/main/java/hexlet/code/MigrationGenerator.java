62efae20c3eca6689fd96d090d47aec2c5659dd2
Mikhail.Chernichenko <mchernichenko@mail.ru>
2021-12-30 21:49:36 +0300

    step #3

--- src/main/java/hexlet/code/MigrationGenerator.java
+++ src/main/java/hexlet/code/MigrationGenerator.java
@@ -8,8 +8,8 @@ import java.io.IOException;
 public class MigrationGenerator {
     public static void main(String[] args) throws IOException {
         DbMigration dbMigration = DbMigration.create();
-        //dbMigration.addPlatform(Platform.H2, "h2");
-        dbMigration.addPlatform(Platform.POSTGRES, "postgres");
+        dbMigration.addPlatform(Platform.H2, "h2");
+       //dbMigration.addPlatform(Platform.POSTGRES, "postgres");
 
         // Для перегенерации удалите xml-файл из src/main/resources/dbmigration/model/
         dbMigration.generateMigration();
